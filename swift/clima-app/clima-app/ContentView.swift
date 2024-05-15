44068db481326f505d464560a9c97310274ef493
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2024-05-15 20:00:00 -0300

    adiciona projeto em swift

---
+++ swift/clima-app/clima-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clima-app
+//
+//  Created by Vinícios Barbosa on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/clima-app/clima-app/clima_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  clima_appApp.swift
+//  clima-app
+//
+//  Created by Vinícios Barbosa on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct clima_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
